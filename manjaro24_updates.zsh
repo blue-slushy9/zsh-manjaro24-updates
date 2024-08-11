4d14cf1fc2a2e9789a87606a6cad0f32b9b8f399
blue-slushy9 <ra.cruz971@gmail.com>
2024-08-11 10:55:26 -0700

    Added some echo statements to make it more clear which updates are being installed at which step.

--- manjaro24_updates.zsh
+++ manjaro24_updates.zsh
@@ -1,10 +1,19 @@
 #!/usr/bin/zsh
 
 # pacman updates
+echo -e "Initializing pacman updates...\n"
 pacman -Syu
+echo -e "pacman updates complete.\n"
+
 
 # snap updates
+echo -e "Initiating snap updates (probably none)...\n"
 snap refresh
+echo -e "snap updates complete.\n"
+
+echo
 
 # yay updates
+echo -e "Initiating yay updates...\n"
 yay -Syu
+echo -e "yay updates complete.\n"
